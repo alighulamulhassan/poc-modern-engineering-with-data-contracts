b468d06869a7812cf3322153f0508753268e1d64
Ghulam Ali <ghulam.ul.hassan.ali@accenture.com>
2025-05-22 11:57:37 +0100

    fixed backend config tsstate file input name as 'key'

--- infra/variables.tf
+++ infra/variables.tf
@@ -12,18 +12,3 @@ variable "apim_name" {
   description = "Name of the existing Azure API Management instance."
   type        = string
 }
-
-variable "storage_account_name" {
-  description = "Storage account name for the Terraform backend state."
-  type        = string
-}
-
-variable "container_name" {
-  description = "Container name for the Terraform backend state."
-  type        = string
-}
-
-variable "terraform_state_key" {
-  description = "Blob key for the Terraform backend state."
-  type        = string
-}
