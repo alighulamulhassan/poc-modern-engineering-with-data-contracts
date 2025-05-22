6e3219d6248df7320e98faa861e286b60d4a53be
Ghulam Ali <ghulam.ul.hassan.ali@accenture.com>
2025-05-22 19:55:09 +0100

    dynamically fetch all open api specs in the repo and create on apim and then apply mock policies to each

--- infra/main.tf
+++ infra/main.tf
@@ -6,33 +6,57 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm" {}
-}
-
-resource "azurerm_api_management_api" "mhra_patient" {
-  name                = "mhra-patient-api"
-  resource_group_name = var.resource_group_name
-  api_management_name = var.apim_name
-  revision            = "1"
-  display_name        = "MHRA Patient API"
-  path                = "patient"
-  protocols           = ["https"]
-  import {
-    content_format = "openapi"
-    content_value  = file("${path.module}/../sample-apis/patient-api.yaml")
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+    }
   }
 }
 
-resource "azurerm_api_management_api_operation_policy" "mock_policy" {
-  api_name            = azurerm_api_management_api.mhra_patient.name
-  operation_id        = "listPatients"
+# Load and parse all OpenAPI specs from sample-apis directory
+locals {
+  api_files = fileset(path.module, "../sample-apis/*.{yaml,yml}")
+  api_specs = {
+    for file in local.api_files : basename(file) => yamldecode(file(file))
+  }
+}
+
+# Create APIs dynamically for each spec file
+resource "azurerm_api_management_api" "apis" {
+  for_each = local.api_specs
+
+  name                = replace(lower(each.value.info.title), " ", "-")
+  resource_group_name = var.resource_group_name
+  api_management_name = var.apim_name
+  revision            = "1"
+  display_name        = each.value.info.title
+  path                = replace(lower(each.value.info.title), " ", "-")
+  protocols           = ["https"]
+  version            = each.value.info.version
+  
+  import {
+    content_format = "openapi"
+    content_value  = file("${path.module}/../sample-apis/${each.key}")
+  }
+}
+
+# Apply mock policies to all APIs
+resource "azurerm_api_management_api_operation_policy" "mock_policies" {
+  for_each = {
+    for api in azurerm_api_management_api.apis : api.name => api
+  }
+  
+  api_name            = each.value.name
   api_management_name = var.apim_name
   resource_group_name = var.resource_group_name
+  # We'll get the operation_id from the generate-mocks.js script output
+  operation_id        = "*"  # This will apply to all operations
   xml_content         = <<XML
 <policies>
   <inbound>
     <base />
-    <mock-response status-code="200" content-type="application/fhir+json">
-      <body>sample</body>
+    <mock-response status-code="200" content-type="application/json">
+      <body>@(context.Variables.GetValueOrDefault<string>("mock-response"))</body>
     </mock-response>
   </inbound>
   <backend>
@@ -46,4 +70,6 @@ resource "azurerm_api_management_api_operation_policy" "mock_policy" {
   </on-error>
 </policies>
 XML
+
+  depends_on = [azurerm_api_management_api.apis]
 }
